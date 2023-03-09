35010a37984eec7336ad8a183959caa0202c93d1
SNEHA CHAUHAN <sneha2003chauhan@gmail.com>
2023-03-09 17:42:03 +0530

    Paste Event Complete

--- src/pages/event.js
+++ src/pages/event.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../Components/Navbar';
 import EventHeroSection from '../Components/EventHeroSection';
 import EventInfoSection from '../Components/EventInfoSection';
+import PastEventSection from '../Components/PastEventSection'
 
 const EventPage = () => {
   return (
@@ -9,6 +10,7 @@ const EventPage = () => {
         <Navbar/>
         <EventHeroSection/>
         <EventInfoSection/>
+        <PastEventSection/>
     </div>
   );
 };
