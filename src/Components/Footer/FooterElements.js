379e06717cdda83a733273ef1cb8fc920f0fdd52
shimjim7 <kamya030btit21@igdtuw.ac.in>
2023-03-11 12:23:00 +0530

    footer updated

--- src/Components/Footer/FooterElements.js
+++ src/Components/Footer/FooterElements.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 import {Link} from 'react-router-dom'
 
 export const FooterContainer = styled.footer`
-  background-color: #101522;
+  background-color: #001522;
 `
 
 export const FooterWrap = styled.div`
--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -9,59 +9,62 @@ const Footer = () => {
          <FooterLinksContainer>
             <FooterLinksWrapper>
                 <FooterLinkItems>
-                    <FooterLinkTitle>About us</FooterLinkTitle>
-                       <FooterLink to="/signin">How it Works</FooterLink>
-                       <FooterLink to="/signin">Testimonials</FooterLink>
-                       <FooterLink to="/signin">Careers</FooterLink>
+                    <FooterLinkTitle>Useful Links</FooterLinkTitle>
+                       <FooterLink to="/signin">Content</FooterLink>
+                       <FooterLink to="/signin">Explore</FooterLink>
+                       {/* <FooterLink to="/signin">Careers</FooterLink>
                        <FooterLink to="/signin">Investors</FooterLink>
-                       <FooterLink to="/signin">Terms of Services</FooterLink>
+                       <FooterLink to="/signin">Terms of Services</FooterLink> */}
                 </FooterLinkItems>
                 <FooterLinkItems>
                     <FooterLinkTitle>Contact Us</FooterLinkTitle>
                        <FooterLink to="/">Contact</FooterLink>
                        <FooterLink to="/">Support</FooterLink>
-                       <FooterLink to="/">Destinations</FooterLink>
+                       {/* <FooterLink to="/">Destinations</FooterLink> */}
                        <FooterLink to="/">Sponsorships</FooterLink>
                 </FooterLinkItems>
             </FooterLinksWrapper>
             <FooterLinksWrapper>
-                <FooterLinkItems>
+                {/* <FooterLinkItems>
                     <FooterLinkTitle>Videos</FooterLinkTitle>
                        <FooterLink to="/">Submit Video</FooterLink>
                        <FooterLink to="/">Ambassdors</FooterLink>
                        <FooterLink to="/">Agency</FooterLink>
                        <FooterLink to="/">Influencer</FooterLink>
-                </FooterLinkItems>
+                </FooterLinkItems> */}
                 <FooterLinkItems>
                     <FooterLinkTitle>Social Media</FooterLinkTitle>
-                       <FooterLink to="/">Instagram</FooterLink>
-                       <FooterLink to="/">Facebook</FooterLink>
-                       <FooterLink to="/">Youtube</FooterLink>
-                       <FooterLink to="/">Twitter</FooterLink>
-                       <FooterLink to="/">LinkedIn</FooterLink>
+                       <FooterLink to="https://www.instagram.com/assetmantle.igdtuw/">Instagram</FooterLink>
+                       {/* <FooterLink to="/">Facebook</FooterLink>
+                       <FooterLink to="/">Youtube</FooterLink> */}
+                       <FooterLink to="https://twitter.com/AM_igdtuw">Twitter</FooterLink>
+                       <FooterLink to="https://www.linkedin.com/company/asset-mantle-igdtuw/">LinkedIn</FooterLink>
                 </FooterLinkItems>
             </FooterLinksWrapper>
          </FooterLinksContainer>
          <SocialMedia>
             <SocialMediaWrap>
                 <SocialLogo to='/'>
-                    UMatter
+                    AssetMantle
                 </SocialLogo>
-                <WebsiteRights>UMatter © {new Date().getFullYear()}
+                <WebsiteRights>Assetmantle © {new Date().getFullYear()}
                 All rights reserved.</WebsiteRights>
                 <SocialIcons>
-                <SocialIconsLink href="/" target="_blank"
+                {/* <SocialIconsLink href="/" target="_blank"
                 area-label="Facebook">
                   <FaFacebook />
-                </SocialIconsLink>
+                </SocialIconsLink> */}
                 <SocialIconsLink href="/" target="_blank"
                 area-label="Instagram">
                   <FaInstagram />
                 </SocialIconsLink>
-                <SocialIconsLink href="/" target="_blank"
+                {/* <SocialIconsLink href="/" target="_blank"
                 area-label="Youtube">
                   <FaYoutube />
-                </SocialIconsLink>
+                </SocialIconsLink><SocialIconsLink href="/" target="_blank"
+                area-label="Youtube">
+                  <FaYoutube />
+                </SocialIconsLink> */}
                 <SocialIconsLink href="/" target="_blank"
                 area-label="Twitter">
                   <FaTwitter />
