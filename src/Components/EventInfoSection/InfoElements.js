7c11c54dca5b1073fa28326361f7a83516259519
Mansi <mansins168@gmail.com>
2023-03-17 17:35:36 +0530

    Revert "modal changes"
    This reverts commit 07940bec211d4c304256a9b61805b2510a1eb7d3.

--- src/Components/EventInfoSection/InfoElements.js
+++ src/Components/EventInfoSection/InfoElements.js
@@ -121,10 +121,10 @@ export const AboutH1=styled.h1`
 /* font-size:5rem;
 color:black;
 margin-bottom:64px; */
-margin-bottom: 20px;
-    font-size: 44px;
-    line-height: 1.2;
-    font-weight: 610;
+margin-bottom: 64px;
+    font-size: 50px;
+    line-height: 1.1;
+    font-weight: 600;
     text-transform: uppercase;
     color: white;
 
@@ -142,17 +142,17 @@ margin-bottom:10px;
 
 `
 export const AboutP=styled.p`
-font-family: 'sans-serif';
+font-family: 'Inter';
 font-style: normal;
-font-weight: 450;
-font-size: 20px;
-line-height: 24px;
+font-weight: 400;
+font-size: 14px;
+line-height: 18px;
 gap: 36px;
-text-align: center;
+text-align:left;
 color:#FFFDFA;
 `;
 export const HeroBtnWrapper = styled.div`
-    margin-top: 8px;
+    margin-top: 32px;
     display: flex;
     flex-direction: row;
     align-items: center;
--- src/Components/EventInfoSection/index.js
+++ src/Components/EventInfoSection/index.js
@@ -2,7 +2,6 @@ import React, {useState} from 'react'
 import blackWhite from'../../images/BlackWhite.png'
 import { AboutContainer, AboutWrapper,  AboutCard, AboutH1, AboutIcon, AboutH2, AboutP,HeroBtnWrapper, Heroimg } from './InfoElements'
 import { Button } from '../ButtonElements'
-import BFF from '../../images/BFF.jpg'
 const About = () => {
   const[hover, setHover] = useState(false)
 
@@ -14,11 +13,10 @@ const About = () => {
       <AboutContainer>
         <AboutH1>Upcoming Events</AboutH1>
         <AboutWrapper>
-        <AboutIcon src={BFF}></AboutIcon>
+        <AboutIcon src={blackWhite}></AboutIcon>
          <AboutCard> 
-          <AboutH1>BFF HACKATHON</AboutH1>
-          <AboutP>Our latest collaboration with DTU AUV brings to you a 24 hour offline hackathon in DTU premises! Mark your calendar on 1st and 2 nd April 2023 and join us with your team to build your solution to the problem statements and have it reviewed by seasoned mentors who will provide you with proper guidance.
-.</AboutP>
+          <AboutH2>Heading/Name of the Event</AboutH2>
+          <AboutP>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</AboutP>
           <HeroBtnWrapper>   
                 <Button  to='signup' onMouseEnter={onHover} onMouseLeave={onHover} primary='true' dark='true'>
                 <Heroimg src={''}/> 
