38a2524b8eadfcce5c9c8a68e62ca330ab3cd72b
Manjusha <114348754+manjusha2912@users.noreply.github.com>
2023-07-23 20:28:34 +0530

    Update index.js of Footer
    replaced content to About
    explore to events 
    removed support from contact us
    sponsorships changed to collaborate

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -10,8 +10,8 @@ const Footer = () => {
             <FooterLinksWrapper>
                 <FooterLinkItems>
                     <FooterLinkTitle>Useful Links</FooterLinkTitle>
-                       <FooterLink to="/signin">Content</FooterLink>
-                       <FooterLink to="/signin">Explore</FooterLink>
+                       <FooterLink to="/signin">About</FooterLink>
+                       <FooterLink to="/signin">Events</FooterLink>
                        {/* <FooterLink to="/signin">Careers</FooterLink>
                        <FooterLink to="/signin">Investors</FooterLink>
                        <FooterLink to="/signin">Terms of Services</FooterLink> */}
@@ -20,9 +20,9 @@ const Footer = () => {
                 <FooterLinkItems>
                     <FooterLinkTitle>Contact Us</FooterLinkTitle>
                        <FooterLink to="/">Contact</FooterLink>
-                       <FooterLink to="/">Support</FooterLink>
-                       {/* <FooterLink to="/">Destinations</FooterLink> */}
-                       <FooterLink to="/">Sponsorships</FooterLink>
+                       {/* <FooterLink to="/">Support</FooterLink>
+                        <FooterLink to="/">Destinations</FooterLink> */}
+                       <FooterLink to="/">Collaborate</FooterLink>
                 </FooterLinkItems>
                 <Gap />
             </FooterLinksWrapper>
