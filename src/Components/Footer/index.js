5ccc43460f19c2f551efff4094df12117d93e7e2
Manjusha <114348754+manjusha2912@users.noreply.github.com>
2023-07-23 20:43:25 +0530

    Update links in index.js in Footer
    Links at footer updated

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -10,8 +10,8 @@ const Footer = () => {
             <FooterLinksWrapper>
                 <FooterLinkItems>
                     <FooterLinkTitle>Useful Links</FooterLinkTitle>
-                       <FooterLink to="/signin">About</FooterLink>
-                       <FooterLink to="/signin">Events</FooterLink>
+                       <FooterLink to="/">About</FooterLink>
+                       <FooterLink to="/event">Events</FooterLink>
                        {/* <FooterLink to="/signin">Careers</FooterLink>
                        <FooterLink to="/signin">Investors</FooterLink>
                        <FooterLink to="/signin">Terms of Services</FooterLink> */}
@@ -49,7 +49,7 @@ const Footer = () => {
                 <SocialLogo >
                    <img src={Logo} height={32}/>
                 </SocialLogo>
-                <WebsiteRights>Assetmantle © {new Date().getFullYear()}
+                <WebsiteRights>Assetmantle © {new Date().getFullYear()}  . 
                 All rights reserved.</WebsiteRights>
                 <SocialIcons>
                 <SocialIconsLink href="https://www.instagram.com/assetmantle.igdtuw/" target="_blank"
