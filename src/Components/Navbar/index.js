7fe550618b1d4a9c17895ca82385c55d56cef072
Apoorva <apoorvagilhotra14@gmail.com>
2023-08-16 02:07:53 +0530

    updated

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,62 +1,64 @@
-import React from 'react'
-import {FaBars} from 'react-icons/fa'
-import Logo2 from '../../images/Logo3.png'
-import {Nav, NavbarContainer,NavImg,  NavMenu, Navitem, NavLinks, MobileIcon, NavLogo, SubLogo} from './NavbarElements';
-import  { useState } from 'react';
+
+
+
+import React from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa'; // Import the close icon as well
+import Logo2 from '../../images/Logo3.png';
+import {
+  Nav,
+  NavbarContainer,
+  NavImg,
+  NavMenu,
+  Navitem,
+  NavLinks,
+  MobileIcon,
+  NavLogo,
+  SubLogo,
+} from './NavbarElements';
+import { useState } from 'react';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const toggle = () =>
-  {
+  const toggle = () => {
     setIsOpen(!isOpen);
   };
+
   return (
     <>
       <Nav>
         <NavbarContainer>
-            <NavImg>
-              <img src={Logo2} height={44}/>
-              <NavLogo>
-                AssetMantle          
-                <SubLogo>Student Chapter IGDTUW </SubLogo>
-              </NavLogo>                              
-            </NavImg>
-                                      
-            <MobileIcon onClick={toggle}>
-                <FaBars/>
-            </MobileIcon>
-            <NavMenu isOpen={isOpen}>
-                <Navitem>
-                    <NavLinks to="/">ABOUT
-                    </NavLinks>
-                </Navitem>
-                <Navitem>
-                    <NavLinks to="/event">EVENTS
-                    </NavLinks>
-                </Navitem>
-               
+          <NavImg>
+            <img src={Logo2} height={44} alt="Logo" />
+            <NavLogo>
+              AssetMantle
+              <SubLogo>Student Chapter IGDTUW</SubLogo>
+            </NavLogo>
+          </NavImg>
 
-                <Navitem>
-                    <NavLinks to="/TeamPage">TEAM
-                    </NavLinks>
-                </Navitem>
-                <Navitem>
-                    <NavLinks to="/faq" >FAQS
-                    </NavLinks>
-                </Navitem>
-               
+          <MobileIcon onClick={toggle}>
+            {isOpen ? <FaTimes /> : <FaBars />} {/* Close icon when open */}
+          </MobileIcon>
+          <NavMenu isOpen={isOpen}>
+            <Navitem>
+              <NavLinks to="/">ABOUT</NavLinks>
+            </Navitem>
+            <Navitem>
+              <NavLinks to="/event">EVENTS</NavLinks>
+            </Navitem>
 
-
-            </NavMenu>
-            {/* <NavBtn>
-                <NavBtnLink to='/signin'>Sign In</NavBtnLink>
-            </NavBtn> */}
+            <Navitem>
+              <NavLinks to="/TeamPage">TEAM</NavLinks>
+            </Navitem>
+            <Navitem>
+              <NavLinks to="/faq">FAQS</NavLinks>
+            </Navitem>
+          </NavMenu>
         </NavbarContainer>
       </Nav>
-   
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
+
