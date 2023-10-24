438fa57b63c5fb258f9dbf965dd0b70ab77ceed8
Agarwalvidu <119511621+Agarwalvidu@users.noreply.github.com>
2023-10-24 20:15:18 +0530

    TeamSidebar

---
+++ src/Components/TeamSidebar/SidebarData.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import TechTeam from '../../images/Integrations.png'
+import EventTeam from '../../images/Mantle_Products.png'
+import MediaTeam from '../../images/Mantle_Creatives.png'
+import ResearchTeam from '../../images/Grants.png'
+import OutreachTeam from '../../images/Community_Initiatives.png'
+import FounderImage from '../../images/Founder.png';
+
+
+
+export const SidebarData=[
+    {
+        title:"Founder",
+        icon:< img src={FounderImage}/>,
+        link:"#founderSection"
+    },
+    {
+        title:"President",
+        icon:< img src={EventTeam}/>,
+        link:"#President"
+    },
+    {
+        title:"Vice President",
+        icon:< img src={EventTeam}/>,
+        link:"#Vice President"
+    },
+    {
+        title:"EM Team",
+        icon:< img src={EventTeam}/>,
+        link:"#emTeamSection"
+    },
+    {
+        title:"Graphics & Media Team",
+        icon:<img src={MediaTeam}/>,
+        link:"#mediaTeamSection"
+    },
+    {
+        title:"Research Team",
+        icon:<img src={ResearchTeam} />,
+        link:"#contentTeamSection"
+    },
+    {
+        title:"Outreach Team",
+        icon:<img src={OutreachTeam}/>,
+        link:"#outreachTeamSection"
+    },
+    {
+        title:"Technical Team",
+        icon:<img src={TechTeam} />,
+        link:"#techTeamSection"
+    }
+] 
--- src/pages/team.js
+++ src/pages/team.js
@@ -2,13 +2,19 @@ import React from 'react'
 import Navbar from '../Components/Navbar'
 import Footer from '../Components/Footer'
 import TeamSection from '../Components/TeamSection'
+import TeamSidebar from '../Components/TeamSidebar/TeamSidebar'
 
 const TeamPage  = () => {
   return (
     <div>
+      
         <Navbar/>
-        <TeamSection/>
+      <div style={{ display: 'flex' }}>
+        <TeamSidebar />
+        <TeamSection />
+      </div>
         <Footer/>
+
     </div>
   )
 }
