fd71cf314d55c2dab42e63514830442a85f2abcf
SNEHA CHAUHAN <sneha2003chauhan@gmail.com>
2023-11-01 16:57:37 +0000

    Events page hove effect

--- src/Components/PastEventSection/index.js
+++ src/Components/PastEventSection/index.js
@@ -12,48 +12,56 @@ import {
   ServicesIcon,
   ServicesLink,
   ServicesContent,
+  Description
 } from './PastEventElements';
 const Services = () => {
   const events = [
     {
       title: 'Orientation and Treasure Hunt',
       link: 'https://www.instagram.com/reel/Cnrgq8Jha-p/',
+      discription: 'Hello I am Description',
       image:
         'https://res.cloudinary.com/djv5kc7as/image/upload/v1688802647/Asset%20Mantle%20-%20Team%202023/Events/Screenshot_300_mgqx8x.png',
     },
     {
       title: 'Find Global Career opportunities on Web3 and Metaverse',
       link: 'https://www.instagram.com/reel/Coy0FTINtEt/',
+      discription: 'Hello I am Description',
       image:
         'https://res.cloudinary.com/djv5kc7as/image/upload/v1688802648/Asset%20Mantle%20-%20Team%202023/Events/Screenshot_301_oeeode.png',
     },
     {
       title: 'Intro to Web3 and its Career Scope',
       link: 'https://www.instagram.com/p/CoeHzBkSLWY/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA==',
+      discription: 'Hello I am Description',
       image:
         'https://res.cloudinary.com/djv5kc7as/image/upload/v1688802647/Asset%20Mantle%20-%20Team%202023/Events/Screenshot_302_pgrxxz.png',
     },
     {
       title: 'BFF Hackathon',
       link: 'https://www.instagram.com/reel/Cs3nO1yN3-6/',
+      discription: 'Hello I am Description',
       image:
         'https://res.cloudinary.com/djv5kc7as/image/upload/v1688802646/Asset%20Mantle%20-%20Team%202023/Events/Screenshot_303_cpvgip.png',
     },
     {
       title: 'FastN Session',
       link: 'https://www.instagram.com/p/CvZ9Ps1yCk_/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA==',
+      discription: 'Hello I am Description',
       image:
         'https://res.cloudinary.com/djv5kc7as/image/upload/v1698596897/Asset%20Mantle%20-%20Team%202023/Events/Screenshot_315_hcwl6o.png',
     },
     {
       title: 'Introduction to Crypto V/S Blockchain and Altcoins',
       link: 'https://www.instagram.com/p/CoeHzBkSLWY/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA==https://www.instagram.com/p/Cv7hyhDscUx/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA==',
+      discription: 'Hello I am Description',
       image:
         'https://res.cloudinary.com/djv5kc7as/image/upload/v1698598489/Asset%20Mantle%20-%20Team%202023/Events/Screenshot_317_nle6lt.png',
     },
     {
       title: 'Ideathon',
       link: 'https://www.instagram.com/p/CoeHzBkSLWY/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA==',
+      discription: 'Hello I am Description',
       image:
         'https://res.cloudinary.com/djv5kc7as/image/upload/v1698597655/Asset%20Mantle%20-%20Team%202023/Events/Screenshot_316_ilf1ht.png',
     },
@@ -62,13 +70,31 @@ const Services = () => {
   const itemsToShow = 5;
   const [currentSlide, setCurrentSlide] = useState(0);
 
+  const [isHover, setIsHover] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHover(true);
+  };
+  
+  const handleMouseLeave = () => {
+    setIsHover(false);
+  };
+
   const renderEvents = () => {
     const startIndex = currentSlide * itemsToShow;
     const endIndex = startIndex + itemsToShow;
     return events.slice(startIndex, endIndex).map((event, index) => (
-      <ServicesCard key={index}>
+      <ServicesCard key={index}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+              // className={isHover ? 'hovered' : ''}
+              isHover={isHover}
+      >
         <ServicesIcon src={event.image} />
         <ServicesContent>
+          <Description>
+            {event.discription}
+          </Description>
           <ServicesH2>
             <b>{event.title}</b>
           </ServicesH2>
@@ -88,7 +114,7 @@ const Services = () => {
     setCurrentSlide(currentSlide - 1);
   };
 
-  
+
   return (
     <ServicesContainer id="services">
       <ServicesH1>Past Events</ServicesH1>
@@ -118,5 +144,5 @@ const Services = () => {
 };
 
 export default Services;
-   
+
 
