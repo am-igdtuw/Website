5c478a83215c0d873f36b25082472e43973bbf6e
Advitiya <advitiya993@gmail.com>
2024-01-07 01:46:29 +0530

    Updated events info

--- src/Components/PastEventSectionPseudo/index.js
+++ src/Components/PastEventSectionPseudo/index.js
@@ -6,21 +6,52 @@ import { InforContainer, InforWrapper, ServicesP } from './PastEventElement.js';
 const events = [
     {
         index:1,
-        heading: "Frontend Development",
+        heading: 'Orientation and Treasure Hunt',
         subheading: "Unlock the Frontend Frontier",
-        icon: Icon1
+        icon:        'https://res.cloudinary.com/djv5kc7as/image/upload/v1688802647/Asset%20Mantle%20-%20Team%202023/Events/Screenshot_300_mgqx8x.png',
+
       },
     {
         index:2,
-        heading: "Another Event Heading",
+        heading: 'Find Global Career opp ortunities on Web3 and Metaverse',
         subheading: "Description for another event goes here.",
-        icon: Icon1
+        icon:        'https://res.cloudinary.com/djv5kc7as/image/upload/v1688802648/Asset%20Mantle%20-%20Team%202023/Events/Screenshot_301_oeeode.png',
+
       },
     {
         index:3,
-        heading: "Yet Another Event",
+        heading: 'Intro to Web3 and its Career Scope',
         subheading: "Description for yet another event.",
-        icon: Icon1
+        icon:        'https://res.cloudinary.com/djv5kc7as/image/upload/v1688802647/Asset%20Mantle%20-%20Team%202023/Events/Screenshot_302_pgrxxz.png',
+
+      },
+      {
+        index:4,
+        heading: 'BFF Hackathon',
+        subheading: "Description for another event goes here.",
+        icon:         'https://res.cloudinary.com/djv5kc7as/image/upload/v1688802646/Asset%20Mantle%20-%20Team%202023/Events/Screenshot_303_cpvgip.png',
+
+      },
+    {
+        index:5,
+        heading: 'FastN Session',
+        subheading: "Description for yet another event.",
+        icon:        'https://res.cloudinary.com/djv5kc7as/image/upload/v1698596897/Asset%20Mantle%20-%20Team%202023/Events/Screenshot_315_hcwl6o.png',
+
+      },
+      {
+        index:6,
+        heading: 'Introduction to Crypto V/S Blockchain and Altcoins',
+        subheading: "Description for another event goes here.",
+        icon:         'https://res.cloudinary.com/djv5kc7as/image/upload/v1698598489/Asset%20Mantle%20-%20Team%202023/Events/Screenshot_317_nle6lt.png',
+
+      },
+    {
+        index:7,
+        heading: 'Ideathon',
+        subheading: "Description for yet another event.",
+        icon:         'https://res.cloudinary.com/djv5kc7as/image/upload/v1698597655/Asset%20Mantle%20-%20Team%202023/Events/Screenshot_316_ilf1ht.png',
+
       }
   ];
 
