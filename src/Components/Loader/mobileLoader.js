0104c1626ca4cbee5b76488d2b13fef31f9099db
hiihimanshi <hiihimanshi@gmail.com>
2024-02-19 20:09:07 +0530

    footer mailbox and links alignment

--- src/Components/Loader/mobileLoader.js
+++ src/Components/Loader/mobileLoader.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import './mobileLoader.css'; 
+// import './mobileLoader.css'; 
 import logo from './AMlogo.png';
 
 function MobileLoader() {
   return (
     <>
-      <img src={logo} alt="Logo" className="AmLogo"/>
-      
+      <img src={logo} alt="Logo" className="AmLogo" />
+
       <input type="checkbox" id="checkbox" />
       {/* <div className="bg"></div> */}
       <div className="dots">
-      
+
         {[...Array(12)].map((_, index) => (
           <div className="dot" key={index}></div>
         ))}
