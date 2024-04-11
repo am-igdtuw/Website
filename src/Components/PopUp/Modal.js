0201b2a6ae982b368d16c426f08d252f577fd36f
Harshita <harshitalkadeep@gmail.com>
2024-04-11 10:40:17 +0530

    popup link redirect to new page

--- src/Components/PopUp/Modal.js
+++ src/Components/PopUp/Modal.js
@@ -1,17 +1,17 @@
-import React, { useEffect , useState } from 'react';
-import Typed from 'typed.js'; // Import typed.js library
-import './Modal.css';
-import PopUp from '../../images/PopUp.png';
-import { FaLinkedin, FaInstagram } from 'react-icons/fa';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useEffect, useState } from "react";
+import Typed from "typed.js"; // Import typed.js library
+import "./Modal.css";
+import PopUp from "../../images/PopUp.png";
+import { FaLinkedin, FaInstagram } from "react-icons/fa";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const OVERLAY = {
-  position: 'fixed',
+  position: "fixed",
   left: 0,
   right: 0,
   top: 0,
   bottom: 0,
-  backgroundColor: 'rgba(0 , 0 ,0 ,0.77)',
+  backgroundColor: "rgba(0 , 0 ,0 ,0.77)",
   zIndex: 1000,
 };
 
@@ -19,46 +19,49 @@ const Modal = (props) => {
   const [showPopup, setShowPopup] = useState(false);
 
   useEffect(() => {
-    const lastPopupTime = localStorage.getItem('lastPopupTime');
+    const lastPopupTime = localStorage.getItem("lastPopupTime");
     const currentTime = Date.now();
 
-    if (!lastPopupTime || (currentTime - parseInt(lastPopupTime, 10)) > 5 * 60 * 1000) {
+    if (
+      !lastPopupTime ||
+      currentTime - parseInt(lastPopupTime, 10) > 5 * 60 * 1000
+    ) {
       // If the popup was never shown or if it was shown more than 5 minutes ago
       setShowPopup(true);
-      localStorage.setItem('lastPopupTime', currentTime.toString());
+      localStorage.setItem("lastPopupTime", currentTime.toString());
     }
   }, []);
 
   useEffect(() => {
     //if (props.trigger) {
-      const initializeSnowfall = () =>{
-      const snowfallContainer = document.querySelector('.snowfall');
-      
-      if(snowfallContainer && snowfallContainer.tagName === 'DIV'){
-      for (let i = 0; i < 17; i++) {
-        const snowflake = document.createElement('div');
-        snowflake.className = 'snowflake';
-        snowflake.style.left = `${Math.random() * 100}%`;
-        snowflake.style.animationDuration = `${Math.random() * 10 + 5}s`;
-        snowfallContainer.appendChild(snowflake);
-      
-   // }
-    }
-    };
-    initializeSnowfall();
+    const initializeSnowfall = () => {
+      const snowfallContainer = document.querySelector(".snowfall");
+
+      if (snowfallContainer && snowfallContainer.tagName === "DIV") {
+        for (let i = 0; i < 17; i++) {
+          const snowflake = document.createElement("div");
+          snowflake.className = "snowflake";
+          snowflake.style.left = `${Math.random() * 100}%`;
+          snowflake.style.animationDuration = `${Math.random() * 10 + 5}s`;
+          snowfallContainer.appendChild(snowflake);
+
+          // }
+        }
+      }
+      initializeSnowfall();
       const options = {
-        strings: ['TWITTER SPACES'],
+        strings: ["TWITTER SPACES"],
         typeSpeed: 100,
         startDelay: 2000,
         showCursor: false,
-        loop: true
+        loop: true,
       };
-      const typed = new Typed('.typed-text', options);
+      const typed = new Typed(".typed-text", options);
 
       return () => {
         typed.destroy();
       };
-    }
+    };
   }, []);
   const handleTopToPage = () => {
     window.scrollTo(0, 0);
@@ -69,13 +72,28 @@ const Modal = (props) => {
       <div className="popup">
         <div className="popup-inner">
           <div className="social-icons">
-            <a href="https://www.instagram.com/assetmerkle.igdtuw/" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://www.instagram.com/assetmerkle.igdtuw/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <FaInstagram />
             </a>
-            <a href="https://twitter.com/AM_igdtuw" target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon="fa-brands fa-x-twitter" style={{ verticalAlign: 'middle' }} />
+            <a
+              href="https://twitter.com/AM_igdtuw"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon
+                icon="fa-brands fa-x-twitter"
+                style={{ verticalAlign: "middle" }}
+              />
             </a>
-            <a href="https://www.linkedin.com/company/asset-merkle/mycompany/" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://www.linkedin.com/company/asset-merkle/mycompany/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <FaLinkedin />
             </a>
           </div>
@@ -86,24 +104,36 @@ const Modal = (props) => {
           <div className="popup-content">
             <img className="popup-img" src={PopUp} alt="Popup" />
             <div className="popup-text">
-              <div className="popup-text1"> ASSETMERKLE Presents   </div>
+              <div className="popup-text1"> ASSETMERKLE Presents </div>
               <div className="popup-text3 typed-text">TWITTER SPACES</div>
-              <div className="popup-text2 "> Explore the decentralized edge.</div> 
-              <button
+              <div className="popup-text2 ">
+                {" "}
+                Explore the decentralized edge.
+              </div>
+              <a
                 className="popup-button"
-                onClick={() => (window.location.href = 'https://twitter.com/AM_igdtuw/status/1750863066177433964')}
+                href="https://twitter.com/AM_igdtuw/status/1750863066177433964"
                 target="_blank"
+                rel="noopener noreferrer"
               >
                 Find it here!
-              </button>
+              </a>
             </div>
           </div>
-          <a href="/event" className="find-more-events" onClick={handleTopToPage}>FIND MORE EVENTS!</a>
+          <a
+            href="/event"
+            className="find-more-events"
+            onClick={handleTopToPage}
+          >
+            FIND MORE EVENTS!
+          </a>
         </div>
       </div>
       <div className="snowfall"></div>
     </>
-  ) : '';
+  ) : (
+    ""
+  );
 };
 
 export default Modal;
