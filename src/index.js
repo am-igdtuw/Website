5ed759f661c7c84b901fd8fa86f377116f6f0d96
Harshita <harshitalkadeep@gmail.com>
2024-04-17 13:25:01 +0530

    vercel analytics

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import GlobalStyles from "./GlobalStyles";
+import { Analytics } from '@vercel/analytics/react';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,6 +11,7 @@ root.render(
   <React.StrictMode>
     <GlobalStyles />
     <App />
+    <Analytics />
   </React.StrictMode>
 );
 
