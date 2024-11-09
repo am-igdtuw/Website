cc7b0d44e22f8d1a3916dfbcb192a8de7d6f6ffc
rose-kaks <rosekakrania@gmail.com>
2024-11-09 13:04:59 +0530

    Adding navbar and sidebar for HackDay

---
+++ src/Components/HackDay/Navhack/index.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import './Navhack.css';
+
+const Navhack = () => {
+  const [activeSection, setActiveSection] = useState('');
+
+  // Function to handle scroll and determine the active section
+  const handleScroll = () => {
+    const sections = document.querySelectorAll("section");
+    const scrollPos = window.scrollY + 100;
+
+    sections.forEach(section => {
+      if (section.offsetTop <= scrollPos && section.offsetTop + section.offsetHeight > scrollPos) {
+        setActiveSection(section.getAttribute("id"));
+      }
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <nav>
+      <div>
+        <h1 className='title'> HACKDAY </h1>
+      </div>
+      <nav className="navbar">
+        <a href="#timeline" className={activeSection === 'timeline' ? 'active' : ''}>Timeline</a>
+        <a href="#desc" className={activeSection === 'desc' ? 'active' : ''}>Description</a>
+        <a href="#rewards" className={activeSection === 'rewards' ? 'active' : ''}>Rewards</a>
+        <a href="#sponsor" className={activeSection === 'sponsor' ? 'active' : ''}>Sponsors</a>
+        <a href="#faq" className={activeSection === 'faq' ? 'active' : ''}>FAQs</a>
+        <a href="#sub" className={activeSection === 'sub' ? 'active' : ''}>Submission Format</a>
+      </nav>
+    </nav>
+  );
+};
+
+export default Navhack;
---
+++ src/Components/HackDay/Sidehack/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Sidehack.css';
+
+const Sidebar = () => {
+  return (
+    <aside className="sidebar">
+        <br></br>
+      <caption>Let's Hack Together!</caption>
+      <hr></hr>
+      <h3>Hosted by</h3>
+      <p>AssetMerkle IGDTUW</p>
+      <h3>Hackathon Mode</h3>
+      <p>OFFLINE</p>
+      <h3>Venue</h3>
+      <p>Event Location</p>
+    </aside>
+  );
+};
+
+export default Sidebar;
