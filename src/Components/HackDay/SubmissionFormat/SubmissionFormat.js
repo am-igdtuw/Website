3a3688e733280319c694587689f45e79800b49e4
Anamika Garg <anamikagarg2906@gmail.com>
2024-11-20 18:22:05 +0530

    Submission format Section added

---
+++ src/Components/HackDay/SubmissionFormat/SubmissionFormat.js
@@ -0,0 +1,57 @@
+import styled from "styled-components"
+
+
+export const Container = styled.div`
+  min-height: 100vh;
+  width: 80%;
+  display: flex;
+  gap: 30px;
+  align-items: flex-start;
+  flex-direction: column;
+  padding-left: 30px;
+  color : white;
+`;
+
+export const MainHeading = styled.h1`
+  font-size: 1.9rem;
+  border-left: 7px solid goldenrod;
+  font-weight: 750;
+  padding-left: 10px;
+  // color:black;
+  margin : 10px 0px 29px 0px;
+`;
+
+export const TrackCon = styled.div`
+  margin-left: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 10px;
+  margin-bottom: 20px;
+  margin-bottom : 10px;
+`;
+
+export const TrackHeading = styled.h2`
+  font-size: 1.5rem;
+  padding: 2px;
+  text-decoration: underline;
+  background: rgba(59, 130, 246, 0.5);
+  font-weight: 650;
+`;
+
+export const List = styled.ol`
+  display: flex;
+  flex-direction: column;
+  padding-left: 15px;
+  gap: 8px;
+
+  ul {
+    gap: 10px;
+  }
+`;
+
+export const ListItem = styled.li`
+  text-align: left;
+  font-size: 17px;
+  list-style-type: disc;
+`;
---
+++ src/Components/HackDay/SubmissionFormat/index.js
@@ -0,0 +1,90 @@
+import React from "react";
+import {
+  Container,
+  List,
+  MainHeading,
+  TrackCon,
+  TrackHeading,
+  ListItem,
+} from "./SubmissionFormat";
+
+const SubmissionFormat = () => {
+  return (
+    <>
+      <Container>
+        <MainHeading>Submission Format</MainHeading>
+
+        <TrackCon>
+          <TrackHeading>Ideathon Submission Format</TrackHeading>
+          <List>
+            <ListItem>
+              {" "}
+              A video demonstration of your project - Time Limit upto 3 minutes.
+            </ListItem>
+            <ListItem>
+              {" "}
+              Presentation with the following mandatory slides (adding anything
+              else is optional) Maximum Slides (8 Slides)
+              <ul>
+                <ListItem>Problem Statement</ListItem>
+                <ListItem>Architecture</ListItem>
+                <ListItem>How your idea is outstand from others</ListItem>
+                <ListItem>Challenges faced</ListItem>
+                <ListItem>Prototype</ListItem>
+              </ul>
+            </ListItem>
+            <ListItem>Prototype link(view access to everyone)</ListItem>
+          </List>
+        </TrackCon>
+
+        <TrackCon>
+          <TrackHeading>Ideathon Submission Format</TrackHeading>
+          <List>
+            <ListItem>
+              {" "}
+              A video demonstration of your project - Time Limit upto 3 minutes.
+            </ListItem>
+            <ListItem>
+              {" "}
+              Presentation with the following mandatory slides (adding anything
+              else is optional) Maximum Slides (8 Slides)
+              <ul>
+                <ListItem>Problem Statement</ListItem>
+                <ListItem>Architecture</ListItem>
+                <ListItem>How your idea is outstand from others</ListItem>
+                <ListItem>Challenges faced</ListItem>
+                <ListItem>Prototype</ListItem>
+              </ul>
+            </ListItem>
+            <ListItem>Prototype link(view access to everyone)</ListItem>
+          </List>
+        </TrackCon>
+
+        <TrackCon>
+          <TrackHeading>Ideathon Submission Format</TrackHeading>
+          <List>
+            <ListItem>
+              {" "}
+              A video demonstration of your project - Time Limit upto 3 minutes.
+            </ListItem>
+            <ListItem>
+              {" "}
+              Presentation with the following mandatory slides (adding anything
+              else is optional) Maximum Slides (8 Slides)
+              <ul>
+                <ListItem>Problem Statement</ListItem>
+                <ListItem>Architecture</ListItem>
+                <ListItem>How your idea is outstand from others</ListItem>
+                <ListItem>Challenges faced</ListItem>
+                <ListItem>Prototype</ListItem>
+              </ul>
+            </ListItem>
+            <ListItem>Prototype link(view access to everyone)</ListItem>
+          </List>
+        </TrackCon>
+      </Container>
+    </>
+  );
+};
+
+export default SubmissionFormat;
