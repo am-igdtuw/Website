bd852b862330bac8122014097e13abb13c4bad23
bhumi-letscode <spk2bhumi@gmail.com>
2025-03-25 13:39:10 +0530

    faq responsiveness

--- src/Components/HackDay/Faq Hackday/FaqElements.js
+++ src/Components/HackDay/Faq Hackday/FaqElements.js
@@ -1,263 +1,231 @@
-import styled from 'styled-components'
+// import styled from 'styled-components'
 
 
-export const HeroContainer = styled.div`
-display: grid;
-grid-template-columns: repeat(2, 1fr);  
-justify-content: center;
-align-items: center;
-padding: 0 30px;
-height: 800px;
-position: relative;
-z-index: 1;
-align-items: center;
+// export const HeroContainer = styled.div`
+// display: grid;
+// grid-template-columns: repeat(2, 1fr);  
+// justify-content: center;
+// align-items: center;
+// padding: 0 30px;
+// height: 800px;
+// position: relative;
+// z-index: 1;
+// align-items: center;
 
-  :before {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    z-index: 2;
-  }
-
-
-  @media screen and (max-width: 1024px) {
-    ${'' /* font-size: 48px; */}
-    padding: 50px 150px;
-  }
-
-  @media screen and (max-width: 768px) {
-    ${'' /* font-size: 40px; */}
-    padding: 50px 50px;
-  }
-
-  @media screen and (max-width: 480px) {
-    ${'' /* font-size: 32px; */}
-    padding: 30px 20px;
-    width: 100%;
-    height: 1600px;
-  }
-
-`;
+//   :before {
+//     content: '';
+//     position: absolute;
+//     top: 0;
+//     left: 0;
+//     right: 0;
+//     bottom: 0;
+//     z-index: 2;
+//   }
 
 
-export const HeroContent = styled.div`
-  z-index: 3;
-  max-width: 2400px;
-  position: absolute;
-  padding: 8px 24px;
-  display: flex;
-  flex-wrap: wrap;
-  flex-direction: column;
-  align-items: center;
-  @media screen and (max-width: 480px) {
-    ${'' /* font-size: 32px; */}
-    padding: 30px 20px;
-    width: 100%;
-  }
-`;
+//   @media screen and (max-width: 1024px) {
+//     ${'' /* font-size: 48px; */}
+//     padding: 50px 150px;
+//   }
 
-export const HeroH1 = styled.h1`
-  text-align: center;
-  display: flex;
-  padding: 50px 50px;
-  padding-top: 1px;
-  width: 200px;
-  //font-family: 'Poppins', sans-serif;
-  font-family: 'Exo 2', sans-serif;
-  
-  ${'' /* font-family: 'Inter'; */}
-  ${'' /* color: #fffdfa; */}
-  font-style: normal;
-  font-size: 52px;
-  font-weight: 500;
-  color: white;
+//   @media screen and (max-width: 768px) {
+//     ${'' /* font-size: 40px; */}
+//     padding: 50px 50px;
+//   }
 
-  @media screen and (max-width: 1024px) {
-    font-size: 48px;
-    padding: 50px 150px;
-    text-align: center;
-    ${'' /* width: 50rem; */}
-  }
+//   @media screen and (max-width: 480px) {
+//     ${'' /* font-size: 32px; */}
+//     padding: 30px 20px;
+//     width: 100%;
+//     height: 1600px;
+//   }
 
-  @media screen and (max-width: 768px) {
-    font-size: 40px;
-    padding: 20px 2px;
-    text-align: center;
-    display: flex;
-    width: 2rem;
-  }
+// `;
 
-  @media screen and (max-width: 480px) {
-    margin-top: 200px;
-    font-size: 32px;
-    padding: 10px 10px;
-    text-align: center;
-    display: flex;
-    width: 5rem;
-    ${'' /* padding-left: 1rem; */}
-  }
-`;
 
-export const Accordian = styled.div`
- display: grid;
-  grid-template-columns: repeat(2, 1fr);
-<<<<<<< HEAD
-  gap: 20px;  
-  width: 100%; 
+// export const HeroContent = styled.div`
+//   z-index: 3;
+//   max-width: 2400px;
+//   position: absolute;
+//   padding: 8px 24px;
+//   display: flex;
+//   flex-wrap: wrap;
+//   flex-direction: column;
+//   align-items: center;
+//   @media screen and (max-width: 480px) {
+//     ${'' /* font-size: 32px; */}
+//     padding: 30px 20px;
+//     width: 100%;
+//   }
+// `;
 
-  @media screen and (max-width: 768px){
-    grid-template-columns: 1fr; 
-  }
+// export const HeroH1 = styled.h1`
+//   display: flex;
+//   border-left: 7px solid goldenrod;
+//   align-items: center;
+//   gap: 10px;
+//   font-size: 1.9rem;
+//   color: white;
+//   margin-bottom: 50px;
+// `;
 
-  @media screen and (max-width: 480px){
-    grid-template-columns: 1fr;  
-=======
-  gap: 20px;  // Adjust the gap between columns if needed
-  width: 100%;  // Adjust for responsiveness
+// export const Accordian = styled.div`
+//  display: grid;
+//   grid-template-columns: repeat(2, 1fr);
+// <<<<<<< HEAD
+//   gap: 20px;  
+//   width: 100%; 
 
-  @media screen and (max-width: 768px){
-    grid-template-columns: 1fr;  // On smaller screens, switch to a single column
-  }
+//   @media screen and (max-width: 768px){
+//     grid-template-columns: 1fr; 
+//   }
 
-  @media screen and (max-width: 480px){
-    grid-template-columns: 1fr;  // Adjust for even smaller screens
->>>>>>> 7ee5f7165e023099c4c40f54c5202407cff0a09f
-  }
+//   @media screen and (max-width: 480px){
+//     grid-template-columns: 1fr;  
+// =======
+//   gap: 20px;  // Adjust the gap between columns if needed
+//   width: 100%;  // Adjust for responsiveness
 
-`
+//   @media screen and (max-width: 768px){
+//     grid-template-columns: 1fr;  // On smaller screens, switch to a single column
+//   }
 
-export const Item = styled.div`
-margin-bottom: 5px;
-padding: 10px 20px;
-width: 100%;  // Adjust the width as needed
+//   @media screen and (max-width: 480px){
+//     grid-template-columns: 1fr;  // Adjust for even smaller screens
+// >>>>>>> 7ee5f7165e023099c4c40f54c5202407cff0a09f
+//   }
 
-@media screen and (max-width: 480px){
-  padding: 10px 0px;
-}
+// `
+
+// export const Item = styled.div`
+// margin-bottom: 5px;
+// padding: 10px 20px;
+// width: 100%;  // Adjust the width as needed
+
+// @media screen and (max-width: 480px){
+//   padding: 10px 0px;
+// }
     
-`
-export const Title = styled.div`
-  border-radius: 24px 24px 0px 0px;
-  padding: 16px;
-  ${'' /* <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Cardo&family=Edu+NSW+ACT+Foundation&display=swap" rel="stylesheet"> */}
-  font-family: 'Poppins', sans-serif;
-  ${'' /* font-family: 'Edu NSW ACT Foundation', cursive;   */}
-  font-weight: 500;
-  font-size: 1.5 rem;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  cursor: pointer;
-  background: #f2af13;
-  color: black;
-  transition: background 0.3s ease;
+// `
+// export const Title = styled.div`
+//   border-radius: 24px 24px 0px 0px;
+//   padding: 16px;
+//   ${'' /* <link rel="preconnect" href="https://fonts.googleapis.com">
+//   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+//   <link href="https://fonts.googleapis.com/css2?family=Cardo&family=Edu+NSW+ACT+Foundation&display=swap" rel="stylesheet"> */}
+//   font-family: 'Poppins', sans-serif;
+//   ${'' /* font-family: 'Edu NSW ACT Foundation', cursive;   */}
+//   font-weight: 500;
+//   font-size: 1.5 rem;
+//   display: flex;
+//   justify-content: space-between;
+//   align-items: center;
+//   cursor: pointer;
+//   background: #f2af13;
+//   color: black;
+//   transition: background 0.3s ease;
 
-  h2 {
-    margin: 0;
-    font-size: 20px;
+//   h2 {
+//     margin: 0;
+//     font-size: 20px;
 
-  }
+//   }
 
-  span {
-    font-size: 20px;
-  }
+//   span {
+//     font-size: 20px;
+//   }
 
-  &:hover {
-    background: #e99d0b;
-  }
+//   &:hover {
+//     background: #e99d0b;
+//   }
   
-`;
+// `;
 
-export const Content = styled.div`
-  color: white;
-  padding: 24px 56px;
-  font-weight: 500;
-  ${'' /* <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Cardo&family=Edu+NSW+ACT+Foundation&display=swap" rel="stylesheet"> */}
-  font-family: 'Poppins', sans-serif;
-  ${'' /* font-family: 'Edu NSW ACT Foundation', cursive; */}
-  height: ${({ selected }) => (selected ? 'auto' : '0')};
-  opacity: ${({ selected }) => (selected ? '1' : '0')};
-  overflow: hidden;
-  transition: all 0.5s ease-in-out;
-  border-radius: 0px 0px 24px 24px;
-  background: linear-gradient(90deg, rgba(255, 198, 64, 0.5) 0%, rgba(169, 169, 169, 0.67) 100%);
-`;
-export const InputBox1 = styled.input`
-  padding: 10px;
-  margin: 0px 20px;
-  width: 761px;
-  height: 40px;
-  border: 3px solid #f2af13;
-  border-radius: 12px;
-  font-size: 16px;
-  margin-left:350px;
-  background-color: #FFFFFF;
-  ::placeholder {
-    color: #000000;
-  }
-  @media screen and (max-width: 768px){
-    width:auto;
-    margin-left: 0;
-  }
+// export const Content = styled.div`
+//   color: white;
+//   padding: 24px 56px;
+//   font-weight: 500;
+//   ${'' /* <link rel="preconnect" href="https://fonts.googleapis.com">
+//   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+//   <link href="https://fonts.googleapis.com/css2?family=Cardo&family=Edu+NSW+ACT+Foundation&display=swap" rel="stylesheet"> */}
+//   font-family: 'Poppins', sans-serif;
+//   ${'' /* font-family: 'Edu NSW ACT Foundation', cursive; */}
+//   height: ${({ selected }) => (selected ? 'auto' : '0')};
+//   opacity: ${({ selected }) => (selected ? '1' : '0')};
+//   overflow: hidden;
+//   transition: all 0.5s ease-in-out;
+//   border-radius: 0px 0px 24px 24px;
+//   background: linear-gradient(90deg, rgba(255, 198, 64, 0.5) 0%, rgba(169, 169, 169, 0.67) 100%);
+// `;
+// export const InputBox1 = styled.input`
+//   padding: 10px;
+//   margin: 0px 20px;
+//   width: 761px;
+//   height: 40px;
+//   border: 3px solid #f2af13;
+//   border-radius: 12px;
+//   font-size: 16px;
+//   margin-left:350px;
+//   background-color: #FFFFFF;
+//   ::placeholder {
+//     color: #000000;
+//   }
+//   @media screen and (max-width: 768px){
+//     width:auto;
+//     margin-left: 0;
+//   }
 
-  @media screen and (max-width: 480px){
-    width:auto;
-    margin-left: 0;
-  }
-`;
-export const InputBox2 = styled.input`
-  padding: 10px;
-  margin: 10px 20px;
-  width: 760px;
-  height: 70px;
-  border: 3px solid #f2af13;
-  border-radius: 20px;
-  font-size: 16px;
-  margin-left:350px;
-  background-color: #FFFFFF;
-  ::placeholder {
-    color: #000000;
-  }
-  @media screen and (max-width: 768px){
-    width:auto;
-    margin-left: 0;
-  }
+//   @media screen and (max-width: 480px){
+//     width:auto;
+//     margin-left: 0;
+//   }
+// `;
+// export const InputBox2 = styled.input`
+//   padding: 10px;
+//   margin: 10px 20px;
+//   width: 760px;
+//   height: 70px;
+//   border: 3px solid #f2af13;
+//   border-radius: 20px;
+//   font-size: 16px;
+//   margin-left:350px;
+//   background-color: #FFFFFF;
+//   ::placeholder {
+//     color: #000000;
+//   }
+//   @media screen and (max-width: 768px){
+//     width:auto;
+//     margin-left: 0;
+//   }
 
-  @media screen and (max-width: 480px){
-    width:auto;
-    margin-left: 0;
-  }
-`;
+//   @media screen and (max-width: 480px){
+//     width:auto;
+//     margin-left: 0;
+//   }
+// `;
 
-export const SubmitButton = styled.button`
-  padding: 10px;
-  margin-right:335px;
-  margin-top: 30px;
-  background-color: #f2af13;
-  color: white;
-  border: 2px solid #f2af13;
-  border-radius: 10px;
-  cursor: pointer;
-  font-size: 16px;
-  @media screen and (max-width: 768px){
-    margin-right:0;
-  }
+// export const SubmitButton = styled.button`
+//   padding: 10px;
+//   margin-right:335px;
+//   margin-top: 30px;
+//   background-color: #f2af13;
+//   color: white;
+//   border: 2px solid #f2af13;
+//   border-radius: 10px;
+//   cursor: pointer;
+//   font-size: 16px;
+//   @media screen and (max-width: 768px){
+//     margin-right:0;
+//   }
 
-  @media screen and (max-width: 480px){
-    margin-right:30px;
-  }
+//   @media screen and (max-width: 480px){
+//     margin-right:30px;
+//   }
 
-`;
-export const FaqBox = styled.div`
-position: relative;
-width:100% ;
-zIndex: 2;
+// `;
+// export const FaqBox = styled.div`
+// position: relative;
+// width:100% ;
+// zIndex: 2;
 
-`;
+// `;
--- src/Components/HackDay/Faq Hackday/index.js
+++ src/Components/HackDay/Faq Hackday/index.js
@@ -1,92 +1,179 @@
-import React from 'react';
-import { useState } from 'react';
-import { HeroContainer, HeroH1, HeroContent, Accordian, Title, Content, Item, InputBox1, InputBox2, SubmitButton, FaqBox } from './FaqElements.js';
-//import DynamicBackground from '../Herosection/DynamicBg.js';import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+export const HeroContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 30px;
+  width: 100%;
+  position: relative;
+  z-index: 1;
+
+  @media screen and (max-width: 1024px) {
+    padding: 50px 30px;
+  }
+
+  @media screen and (max-width: 768px) {
+    padding: 50px 20px;
+  }
+
+  @media screen and (max-width: 480px) {
+    padding: 30px 10px;
+  }
+`;
+
+export const HeroContent = styled.div`
+  z-index: 3;
+  max-width: 1200px;
+  width: 100%;
+  padding: 8px 24px;
+  display: flex;
+  flex-direction: column;
+  
+  @media screen and (max-width: 480px) {
+    padding: 8px 10px;
+  }
+`;
+
+export const HeroH1 = styled.h1`
+  display: flex;
+  align-items: center;
+  border-left: 7px solid goldenrod;
+  padding-left: 10px;
+  font-size: 1.9rem;
+  color: white;
+  margin-bottom: 30px;
+  align-self: flex-start;
+
+  @media screen and (max-width: 480px) {
+    font-size: 1.5rem;
+    margin-bottom: 20px;
+  }
+`;
+
+export const Accordian = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 15px;
+  width: 100%;
+`;
+
+export const Item = styled.div`
+  width: 100%;
+`;
+
+export const Title = styled.div`
+  border-radius: 24px 24px 0px 0px;
+  padding: 16px;
+  font-family: 'Poppins', sans-serif;
+  font-weight: 500;
+  font-size: 1rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  cursor: pointer;
+  background: #f2af13;
+  color: black;
+  transition: background 0.3s ease;
+
+  h2 {
+    margin: 0;
+    font-size: 16px;
+    flex-grow: 1;
+  }
+
+  span {
+    font-size: 20px;
+    margin-left: 10px;
+  }
+
+  &:hover {
+    background: #e99d0b;
+  }
+
+  @media screen and (max-width: 480px) {
+    padding: 12px;
+    
+    h2 {
+      font-size: 14px;
+    }
+  }
+`;
+
+export const Content = styled.div`
+  color: white;
+  padding: 24px;
+  font-weight: 500;
+  font-family: 'Poppins', sans-serif;
+  height: ${({ selected }) => (selected ? 'auto' : '0')};
+  opacity: ${({ selected }) => (selected ? '1' : '0')};
+  overflow: hidden;
+  transition: all 0.5s ease-in-out;
+  border-radius: 0px 0px 24px 24px;
+  background: linear-gradient(90deg, rgba(255, 198, 64, 0.5) 0%, rgba(169, 169, 169, 0.67) 100%);
+  font-size: 14px;
+
+  @media screen and (max-width: 480px) {
+    padding: 16px;
+  }
+`;
 
 const data = [
-    {
-        question: 'When was AssetMerkle IGDTUW established?',
-        answer: 'AssetMerkle IGDTUW was established in January 2023 at IGDTUW'
-    },
-    {
-        question: 'What is AssetMerkle?',
-        answer: 'AssetMerkle IGDTUW is the first Web3-based club at IGDTUW. Our society intends to provide a platform for students to familiarize themselves with the professional world and gain practical experience in blockchain and Web3 technology.'
-    },
-    {
-        question: 'What is the aim of AssetMerkle IGDTUW?',
-        answer: 'Our aim is to bridge the gap between academic learning and real-world challenges in the field of blockchain and Web3 technology. With the growing importance of Web3 technology in the industry, we believe it is crucial for students to gain practical skills in this field.'
-    },
-    {
-        question: 'Can I be a part of AssetMerkle IGDTUW?',
-        answer: 'Yes, you can join our WhatsApp community and be a part of AssetMerkle IGDTUW.'
-    },
-    {
-        question: 'I am from BBA, can I still be a part of this community?',
-        answer: 'Absolutely! AssetMerkle encourages participation from individuals of all academic backgrounds, including BBA, and values the diverse skills they bring to the community.'
-    },
-    {
-        question: 'I know nothing about web3, can I be a part of AssetMerkle IGDTUW?',
-        answer: 'Certainly! AssetMerkle IGDTUW welcomes individuals with varying levels of knowledge, and if you are new to web3, we have got you covered. We offer mentor sessions, workshops, and other resources to help clear up your doubts and ensure that everyone, regardless of their background, can actively participate and learn within our community.'
-    },
-
+  {
+    question: 'When was AssetMerkle IGDTUW established?',
+    answer: 'AssetMerkle IGDTUW was established in January 2023 at IGDTUW'
+  },
+  {
+    question: 'What is AssetMerkle?',
+    answer: 'AssetMerkle IGDTUW is the first Web3-based club at IGDTUW. Our society intends to provide a platform for students to familiarize themselves with the professional world and gain practical experience in blockchain and Web3 technology.'
+  },
+  {
+    question: 'What is the aim of AssetMerkle IGDTUW?',
+    answer: 'Our aim is to bridge the gap between academic learning and real-world challenges in the field of blockchain and Web3 technology. With the growing importance of Web3 technology in the industry, we believe it is crucial for students to gain practical skills in this field.'
+  },
+  {
+    question: 'Can I be a part of AssetMerkle IGDTUW?',
+    answer: 'Yes, you can join our WhatsApp community and be a part of AssetMerkle IGDTUW.'
+  },
+  {
+    question: 'I am from BBA, can I still be a part of this community?',
+    answer: 'Absolutely! AssetMerkle encourages participation from individuals of all academic backgrounds, including BBA, and values the diverse skills they bring to the community.'
+  },
+  {
+    question: 'I know nothing about web3, can I be a part of AssetMerkle IGDTUW?',
+    answer: 'Certainly! AssetMerkle IGDTUW welcomes individuals with varying levels of knowledge, and if you are new to web3, we have got you covered. We offer mentor sessions, workshops, and other resources to help clear up your doubts and ensure that everyone, regardless of their background, can actively participate and learn within our community.'
+  },
 ];
 
 const FAQHackday = () => {
-    const [selected, setSelected] = useState(null);
-    const [email, setEmail] = useState('');
-    const [doubt, setDoubt] = useState('');
+  const [selected, setSelected] = useState(null);
 
-    const handleEmailChange = (e) => {
-        setEmail(e.target.value);
-    };
+  const toggle = (i) => {
+    setSelected(selected === i ? null : i);
+  };
 
-    const handleDoubtChange = (e) => {
-        setDoubt(e.target.value);
-    };
-
-    const handleSubmission = () => {
-        // You can handle the submission of email and doubt here
-        console.log('Email:', email);
-        console.log('Doubt:', doubt);
-
-        // Reset the input values after submission if needed
-        setEmail('');
-        setDoubt('');
-    };
-
-    const toggle = (i) => {
-        if (selected === i) {
-            setSelected(null);
-        } else {
-            setSelected(i);
-        }
-    };
-
-    return (
-        <>
-            <HeroContainer>
-                {/* <DynamicBackground  /> */}
-                <HeroContent>
-                    <HeroH1>FAQs</HeroH1>
-
-                    <Accordian>
-                        {data.map((item, i) => (
-                            <Item key={i}>
-                                <Title onClick={() => toggle(i)}>
-                                    <h2>{item.question}</h2>
-                                    <span>{selected === i ? '-' : '+'}</span>
-                                </Title>
-                                <Content selected={selected === i}>
-                                    {item.answer}
-                                </Content>
-                            </Item>
-                        ))}
-                    </Accordian>
-                </HeroContent>
-            </HeroContainer>
-        </>
-    );
+  return (
+    <HeroContainer>
+      <HeroContent>
+        <HeroH1>FAQs</HeroH1>
+        
+        <Accordian>
+          {data.map((item, i) => (
+            <Item key={i}>
+              <Title onClick={() => toggle(i)}>
+                <h2>{item.question}</h2>
+                <span>{selected === i ? '-' : '+'}</span>
+              </Title>
+              <Content selected={selected === i}>
+                {item.answer}
+              </Content>
+            </Item>
+          ))}
+        </Accordian>
+      </HeroContent>
+    </HeroContainer>
+  );
 };
 
 export default FAQHackday;
